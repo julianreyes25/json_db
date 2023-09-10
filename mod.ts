0390051f66f9a9317dc71b50bc8b3af038ff73e2
JulianR23 <reyesjulian2530@gmail.com>
2023-09-10 16:43:37 -0500

    First version, 0.0.1

--- mod.ts
+++ mod.ts
@@ -1,1 +1,122 @@
-export default 'Module in dev' 
+// I'm a begginer on Deno, literally this is my first module so if there's a error, you can contact me and fix that error
+// deno-lint-ignore-file no-explicit-any
+import { existsSync } from 'https://deno.land/std@0.201.0/fs/mod.ts';
+import { CreateOptions, SchemaType } from './types.ts';
+import { error, parsers, typeOf } from './util.ts';
+
+export class Database<schema> {
+    /***
+    * Creates a new JSON Database\
+    * Requires `allow-read` and `allow-write` permission
+    */
+    constructor (options: CreateOptions<schema>) {
+        if (!options) error('The options are required to create a JSON Database');
+        if (typeof options.name !== 'string') error(`Expected "string" but got "${typeOf(options.name)}"`);
+        if (!options.schema) error('The "schema" option is required');
+        
+        const path = options.path ?? `${Deno.cwd()}/databases/${options.name}.json`;
+        const schema: any = options.schema;
+
+        if (!existsSync(path)) {
+            if (!options.path) {
+                Deno.mkdirSync(`${Deno.cwd()}/databases`, { recursive: true })
+                Deno.writeTextFileSync(path, `{"${options.name}":[]}`)
+            }
+            else error(`The file "${path}" does not exists`)
+        }
+
+        Object.entries(options.schema).forEach(([key, value]) => {
+            const v: any = value;
+            schema[key] = {
+                 type: v.type ?? 4,
+                 required: v.required ?? false
+            }
+        });
+
+        let src_code: string;
+        try {
+            src_code = Deno.readTextFileSync(path)
+        } catch {
+            Deno.writeTextFileSync(path, `{"${options.name}":[]}`)
+            src_code = Deno.readTextFileSync(path)
+        }
+
+        this.#schema = schema;
+        this.#updateFile = () => {
+            const obj: any = {};
+            obj[options.name] = this.#db;
+            Deno.writeTextFileSync(path, JSON.stringify(obj))
+        }
+        this.#db = JSON.parse(src_code)[options.name];
+        this.lenght = this.#db.length;
+
+        return this;
+    }
+
+    /***
+     * Finds, and returns an object if it exits\
+     * If the reference is duplicated returns the latest document
+     */
+    findOne (object: schema): schema | null {
+        let result = null;
+
+        for (const doc of this.#db) {
+            Object.entries(doc).forEach(([k, v]) => {
+                Object.entries(object as any).forEach(([k_, v_]) => {
+                    if (k === k_ && v === v_) result = doc;
+                });
+            });
+        }
+
+        return result;
+    }
+
+    /***
+     * Create a new document in the database
+     */
+    new (object: schema) {
+        Object.entries(object as any).forEach(([k, v]) => {
+            const schema_ = this.#schema[k];
+            const parser = (parsers as any)[SchemaType[schema_.type].toLowerCase()];
+            parser(v);
+        });
+
+        const doc = this.findOne(object);
+
+        doc ? 0 : this.#db.push(object);
+        this.#updateFile();
+    }
+
+    /***
+     * Returns a map
+     */
+    map (callback: (object: schema) => unknown): schema[] {
+        const schema: schema[] = [];
+
+        for (const doc of this.#db) {
+            schema.push(callback(doc) as any);
+        }
+
+        return schema;
+    }
+
+
+
+    #updateFile: () => void;
+
+    #db: any[];
+    #schema: any;
+    lenght: number;
+}
+
+/***
+ * Creates a new JSON Database\
+ * Requires `allow-read` and `allow-write` permission
+ */
+export function createDB<schema> (options: CreateOptions<schema>) {
+    if (new.target) error('"createDB" is not a NewableFunction');
+    return new Database<schema>(options);
+}
+
+export default createDB;
+export * from './types.ts';
---
+++ util.ts
@@ -0,0 +1,48 @@
+import { red, yellow } from 'https://deno.land/std@0.201.0/fmt/colors.ts';
+
+/***
+ * Creates a new Error
+ */
+function error (msg: string, name?: string) {
+    console.log(red(`[${name ? name : "ERROR"}]`), msg);
+    Deno.exit(1);
+}
+/***
+ * Creates a new Warn
+ */
+function warn (msg: string, name?: string) {
+    console.log(yellow(`[${name ? name : "WARN"}]`), msg);
+}
+/***
+ * A fixed `typeof` function\
+ * Examples:
+ * ```ts
+ * typeOf([]) // "array"
+ * typeOf(null) // "null"
+ * typeOf({}.x) // "undefined"
+ * ```
+ */
+function typeOf (value: unknown) {
+    if (value === null) return 'null';
+    if (Array.isArray(value)) return 'array';
+    return typeof value;
+}
+/***
+ * The parsers used for the type checking of the schema
+*/
+const parsers = {
+    expected: (expectedType: ReturnType<typeof typeOf>, value: unknown) => {
+        if (typeOf(value) !== expectedType) error(`Expected "${expectedType}", but got "${typeOf(value)}"`, 'TypeError');
+        return value;
+    },
+    string: (value: unknown) => parsers.expected('string', value),
+    number: (value: unknown) => parsers.expected('number', value),
+    object: (value: unknown) => parsers.expected('object', value),
+    array: (value: unknown) => parsers.expected('array', value),
+    any: (value: unknown) => {
+        warn('You are using the SchemaType any', 'TypeWarn');
+        return value;
+    }
+}
+
+export { warn, error, typeOf, parsers }
